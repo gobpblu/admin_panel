18ec0509002d6f48fae7591d6b97244c492252f9
Max Abramov <gobpo2002@gmail.com>
2025-11-21 20:16:41 +0600

    Lesson 25

--- lib/features/media/screens/responsive_screens/media_desktop_screen.dart
+++ lib/features/media/screens/responsive_screens/media_desktop_screen.dart
@@ -1,16 +1,17 @@
 import 'package:ecommerce_admin_panel/common/widgets/breadcrumbs/breadcrumb_with_heading.dart';
 import 'package:ecommerce_admin_panel/utils/constants/app_sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
 
 class MediaDesktopScreen extends StatelessWidget {
   const MediaDesktopScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(AppSizes.defaultSpace),
+          padding: const EdgeInsets.all(AppSizes.defaultSpace),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -20,9 +21,15 @@ class MediaDesktopScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   // Breadcrumbs
-                  BreadcrumbsWithHeading(
+                  const BreadcrumbsWithHeading(
                     heading: 'Media',
                     breadcrumbItems: [],
+                  ),
+
+                  ElevatedButton.icon(
+                    onPressed: () {},
+                    icon: const Icon(Iconsax.cloud_add),
+                    label: const Text('Upload Images'),
                   ),
                 ],
               ),
